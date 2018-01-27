f62d5596ee4008373f507a15cfea73242d039cc7
Sai <austin.kiran@gmail.com>
2018-01-27 21:50:14 +0100

    added logging for Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,8 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.client.HttpRequest;
 import org.eclipse.jetty.client.HttpResponse;
+
 
 import static spark.Spark.get;
 import static spark.Spark.post;
@@ -7,14 +10,22 @@ import static spark.Spark.post;
 
 public class Application {
 
+    private static final Logger logger = LogManager.getLogger("Application");
+
     public static void main(String[] args) {
+        logger.info("Starting spin text API");
+        logger.info("Initializing dictionary");
         Dictionary dict = new Dictionary();
         try {
             dict.initialize();
+            logger.info("Initialization successful");
         } catch (Exception e){
-
+            logger.error("Initialization failed");
+            logger.error(e);
         }
+
         get("/hello", (request, response) -> {
+            logger.info("New request received");
             return "world";
         });
         post("/sg", (request, response) -> {
@@ -28,44 +39,36 @@ public class Application {
         get("/sg/:id", (request, response) -> {
             int id = Integer.parseInt(request.params(":id"));
             Session ss = Session.getGame(id);
-            return Dictionary.shuffle(ss.getMysteryWord());
+            logger.info("Session ID : " + id);
+            String word =Dictionary.shuffle(ss.getMysteryWord());
+            logger.info("Word : " + word);
+            return word;
         });
 
         post("/sg/input", (request, response) -> {
-            //request.queryParams();
-
             String guess = request.queryParams("input");
-
             int id = Integer.parseInt(request.queryParams("id"));
-
-            //System.out.println(request.queryParams());
-           // String guess = request.queryParams("input");
-
+            logger.info("Session ID : " + id);
+            logger.info("Guess : " + guess);
             Session ss = Session.getGame(id);
-
-            System.out.println("Actual word " + ss.getMysteryWord());
-
             if (!ss.wordAlreadyGuessed(guess.toLowerCase()) && dict.wordExists(guess.toLowerCase())) {
-                System.out.println(guess + " Exists");
+                logger.info(guess + " Exists");
                 ss.addNewGuess(guess.toLowerCase());
                 ss.updateScore(guess.length());
-            }
 
+            } else {
+                logger.info("word does not exist");
+            }
             return ss.getScore();
         });
 
         post("/sg/complete", (request, response) -> {
-            //request.queryParams();
-
-
             int id = Integer.parseInt(request.queryParams("id"));
-
-            //System.out.println(request.queryParams());
-            // String guess = request.queryParams("input");
-
+            logger.info("Session ID : " + id);
             Session ss = Session.getGame(id);
-
-            return ss.gameCompleted();
+            boolean gameCompleted = ss.gameCompleted();
+            logger.info("Game completed : " + gameCompleted);
+            return gameCompleted;
         });
 
     }
