818d21ffe1bd2397f192acc3246905c97df6abcf
Sai <austin.kiran@gmail.com>
2018-01-27 21:55:16 +0100

    logging for session

--- src/main/java/Session.java
+++ src/main/java/Session.java
@@ -1,3 +1,6 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -6,18 +9,27 @@ import java.util.Map;
 
 public class Session {
     private static int counter = 1;
+
+    private static final Logger logger = LogManager.getLogger("Session");
     private static Map<Integer, Session> allSessions = new HashMap<>();
     private int gameId;
     private String mysteryWord;
     private int score;
-
     private List<String> guessedWords;
+
+    public Session(String mysteryWord) {
+        gameId = counter++;
+        this.mysteryWord = mysteryWord;
+        guessedWords = new ArrayList<>();
+        score = 0;
+        allSessions.put(gameId, this);
+    }
 
     public void addNewGuess(String guess) {
         guessedWords.add(guess);
     }
-
     // Game is completed, if either the mysterword is guessed or 20 points
+
     public boolean gameCompleted() {
         if(guessedWords.equals(mysteryWord) || score>=10) {
             return true;
@@ -38,14 +50,6 @@ public class Session {
         return gameId;
     }
 
-    public Session(String mysteryWord) {
-        gameId = counter++;
-        this.mysteryWord = mysteryWord;
-        guessedWords = new ArrayList<>();
-        score = 0;
-        allSessions.put(gameId, this);
-    }
-
     public int updateScore(int score) {
         this.score += score;
         return this.score;
@@ -53,9 +57,7 @@ public class Session {
 
     public int startGame() {
         return gameId;
-
     }
-
 
     public static Session getGame(int sessionID) {
         return allSessions.get(sessionID);
